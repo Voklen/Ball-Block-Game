4bd1f22d616b4150b0c19a8c6fb81de97f3b1db9
Alex <Alex.Gorichev@protonmail.com>
2020-04-08 21:58:28 +0100

    Proper Formatting

--- Scripts/Igg.gd
+++ Scripts/Igg.gd
@@ -7,12 +7,16 @@ var acceleration = 1
 var maxSpeed = 7
 var gameOverAt_y = -20
 
+
 func _ready():
 	Level.blocksFallen = 0
+
+
 func _physics_process(delta):
 	if translation.y < gameOverAt_y:
 		Level.stop = 1
 		get_tree().change_scene("res://Lose.tscn")
+	
 	if Input.is_action_pressed("ui_right"):
 		if velocity.x < maxSpeed:
 		 velocity.x += acceleration
@@ -25,11 +29,14 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_up"):
 		if velocity.z > -maxSpeed:
 		 velocity.z -= acceleration
+	
 	velocity.x = lerp(velocity.x,0,friction)
 	velocity.z = lerp(velocity.z,0,friction)
 	rotateBall(velocity.z,0,-velocity.x)
 	velocity += Vector3(0,-gravity,0) * delta
 	move_and_slide(velocity)
+
+
 func rotateBall(x,y,z):
 	$ MeshInstance.rotate_x(deg2rad(x))
 	$ MeshInstance.rotate_y(deg2rad(y))
